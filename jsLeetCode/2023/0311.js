2264494b253bf76bf921b7b5f59e7857d7a3c13e
徐弈 <1298496555@qq.com>
2023-03-12 13:49:09 +0800

    0311

--- jsLeetCode/2023/0311.js
+++ jsLeetCode/2023/0311.js
@@ -1,45 +1,48 @@
-// 1590 使数组和能被 P 整除
-// 给你一个正整数数组 nums ,请你移除最短子数组，使得剩余元素和能被p整除
-// 不允许把整个数组都移除，不行则返回 -1
-// 子数组定义为原数组中连续的一组元素
+// 面试 17.05 字母与数字
+// 给定一个放有字母和数字的数组，找到最长的子数组，且包含字母和数字的个数相同
+// 返回该子数组，若存在多个最长子数组，返回左端点下班值最小的数组，若不存在，返回空数组
 
 /**
- * @param {number[]} nums
- * @param {number} p
- * @return {number}
+ * @param {string[]} array
+ * @return {string[]}
  */
 
-// 求出前缀和
-// 连续数字的和可以通过前缀和求出
-var minSubarray = function (nums, p) {
-  const n = nums.length;
-  const sumArray = new Array(n).fill(0);
-  sumArray[0] = nums[0];
-  for (let i = 1; i < n; i++) {
-    sumArray[i] = sumArray[i - 1] + nums[i];
-  }
-  if (sumArray[n - 1] % p == 0) {
-    return 0;
-  }
-  let count = 1;
-  while (count < n) {
-    for (let i = n - 1; i >= count; i--) {
-      if ((sumArray[n - 1] - sumArray[i] + sumArray[i - count]) % p == 0) {
-        return count;
-      }
+// 前缀和，字母为1 数字为-1
+// 寻找转换后的元素和为0的最长子数组
+// 两个下标对应的前缀和相等，则两者的元素和为0
+var findLongestSubarray = function (array) {
+  const n = array.length;
+  const indices = new Map
+  indices.set(0,-1)
+  let sum = 0
+  let maxLength = 0
+  let startIndex = -1
+  for(let i=0;i<n;i++){
+    if(isNumber(array[i])){
+        sum--
+    }else{
+        sum++
     }
-    if ((sumArray[n - 1] - sumArray[count - 1]) % p == 0) {
-      return count;
+    if(indices.has(sum)){
+        // 已经推入过一次，那么此次可以计算出子数组
+        const firstIndex = indices.get(sum)
+        if(i-firstIndex > maxLength){
+            maxLength = i-firstIndex
+            startIndex = firstIndex+1
+        }
+    }else{
+        indices.set(sum,i)
     }
-    count++;
   }
-  if (count == n) {
-    return -1;
-  } else {
-    return count;
+  if(maxLength ==0){
+    return []
   }
+  return [...array.slice(startIndex,startIndex+maxLength)]
 };
 
-nums = [8, 32, 31, 18, 34, 20, 21, 13, 1, 27, 23, 22, 11, 15, 30, 4, 2];
-p = 148;
-console.log(minSubarray(nums, p));
+const isNumber = (ch)=>{
+    return Number(ch) === Number(ch) 
+}
+
+array =["A","1"]
+console.log(findLongestSubarray(array));
